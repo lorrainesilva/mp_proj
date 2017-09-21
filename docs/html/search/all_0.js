b33190827f018d9de0785cc979012fbde0888499
Ingrid Lorraine <ingridsilva71@gmail.com>
2017-09-20 23:17:59 -0300

    Adição da documentação.

---
+++ docs/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['busca_5fdelimitadores',['busca_delimitadores',['../string__soma_8hpp.html#a7db9d324ca3aca0314c136f24db5e0d2',1,'busca_delimitadores(std::string *entrada, std::vector&lt; std::string &gt; *vetor):&#160;string_soma.cpp'],['../string__soma_8cpp.html#a9ccefb78a6144c11f9ca7fa157319bbd',1,'busca_delimitadores(std::string *entrada, std::vector&lt; std::string &gt; *vetor):&#160;string_soma.cpp']]]
+];
---
+++ docs/html/search/functions_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['soma_5fstring',['soma_string',['../string__soma_8hpp.html#a5e05ec4700872c44ab0b5d73463e0d63',1,'soma_string(char *string_entrada):&#160;string_soma.cpp'],['../string__soma_8cpp.html#a5584a11d7a161d9a35a2e76cb51681bd',1,'soma_string(char *string_entrada):&#160;string_soma.cpp']]],
+  ['somatorio',['somatorio',['../string__soma_8hpp.html#aafd15287f737f9d5f3dd8cc241a2d140',1,'somatorio(std::string *entrada, std::vector&lt; std::string &gt; *vetor):&#160;string_soma.cpp'],['../string__soma_8cpp.html#a98a88d9d604cc46fbc51ddfee54565d1',1,'somatorio(std::string *entrada, std::vector&lt; std::string &gt; *vetor):&#160;string_soma.cpp']]]
+];
